c2c278f7c6198697504c3548aabbde948b099efa
akash1810 <akash1810@users.noreply.github.com>
2023-09-26 11:16:58 +0100

    feat: Run HelloWorld container

--- README.md
+++ README.md
@@ -10,4 +10,8 @@ npx @guardian/cdk@latest new \
 ```
 - [x] Setup the project in CI (https://docs.github.com/en/actions/using-workflows/workflow-syntax-for-github-actions) and CD
 - [ ] Run the Dockerfile in AWS (via GuCDK)
+   - [ ] Run the HelloWorld image in AWS
+   - [ ] Run our image in AWS
+       - [ ] Make the repo public
+       - [ ] Publish image to GitHub Packages during CI
 - [ ] Profit?
--- lib/interactive-tilemaker.ts
+++ lib/interactive-tilemaker.ts
@@ -1,9 +1,26 @@
 import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
 import { GuStack } from "@guardian/cdk/lib/constructs/core";
 import type { App } from "aws-cdk-lib";
+import { Compatibility, ContainerImage, CpuArchitecture, OperatingSystemFamily, TaskDefinition } from "aws-cdk-lib/aws-ecs";
 
 export class InteractiveTilemaker extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
     super(scope, id, props);
+
+    const task = new TaskDefinition(this, "TileMakerTask", {
+      compatibility: Compatibility.FARGATE,
+      runtimePlatform: {
+        operatingSystemFamily: OperatingSystemFamily.LINUX,
+        cpuArchitecture: CpuArchitecture.X86_64
+      },
+      cpu: (1 * 1024).toString(),
+      memoryMiB: (2 * 1024).toString()
+    });
+
+
+    task.addContainer('HelloWorldContainer', {
+      image: ContainerImage.fromRegistry("docker/hello-world"),
+      memoryLimitMiB: 2048,
+    });
   }
 }
