120d83a2b5dbfca47bff6fc5e699971723be691f
akash1810 <akash1810@users.noreply.github.com>
2023-09-26 11:42:43 +0100

    fix: Define cluster

--- lib/interactive-tilemaker.ts
+++ lib/interactive-tilemaker.ts
@@ -1,11 +1,13 @@
 import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
 import { GuStack } from "@guardian/cdk/lib/constructs/core";
 import type { App } from "aws-cdk-lib";
-import { Compatibility, ContainerImage, CpuArchitecture, OperatingSystemFamily, TaskDefinition } from "aws-cdk-lib/aws-ecs";
+import { Compatibility, ContainerImage, CpuArchitecture, OperatingSystemFamily, TaskDefinition, Cluster, FargateService } from "aws-cdk-lib/aws-ecs";
 
 export class InteractiveTilemaker extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
     super(scope, id, props);
+
+    const cluster = new Cluster(this, "TileMakerCluster");
 
     const task = new TaskDefinition(this, "TileMakerTask", {
       compatibility: Compatibility.FARGATE,
@@ -22,5 +24,10 @@ export class InteractiveTilemaker extends GuStack {
       image: ContainerImage.fromRegistry("docker/hello-world"),
       memoryLimitMiB: 2048,
     });
+
+    const service = new FargateService(this, "TileMakerService", {
+      cluster,
+      taskDefinition: task,
+    })
   }
 }
